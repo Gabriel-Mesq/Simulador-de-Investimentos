b243eabdcfe779b597ff4a742a5e98161717314f
Gabriel Mesquita <90516048+Gabriel-Mesq@users.noreply.github.com>
2023-06-21 16:31:54 -0300

    Comparação com CDI

--- tempoParaMeta.py
+++ tempoParaMeta.py
@@ -1,12 +1,14 @@
 '''
 Considerações importantes:
 
-O código considera o reinvestimento de proventos.
-
 Cáculo de rendimento mensal feito com (rendimento anual / 12) é conta de padeiro.
 A fórmula correta dos juros compostos é: M = P * (1 + r/n)^(n*t)
 
+O código considera o reinvestimento de proventos, o que é essencial para dar inicio ao "efeito bola de neve".
 A inflação não está sendo considerada.
+
+Essa simulação deixa claro que consistência, especialmente no inicio onde os aportes são uma parcela significativa do total, é a chave para geração de renda passiva.
+Ao investir com disciplina, flutuações de curto prazo são mitigadas e se percebe os benefícios do crescimento composto.
 '''
 
 import matplotlib.pyplot as plt
@@ -48,7 +50,20 @@ def construcao_grafico(lista_aportado, lista_redimento, lista_renda_passiva):
 
     plt.show() 
 
-def tempo_para_renda_desejada(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, renda_passiva_desejada, INTERVALO_COLUNAS):
+def comparar_ao_cdi(valor_inicial, aporte_mensal, anos, meses):
+
+    cdi_mensal = pow((1+TAXA_CDI), (1/12))
+    saldo_cdi =  valor_inicial*cdi_mensal
+
+    tempo = anos*12 + meses
+    while tempo > 1:
+
+        saldo_cdi = (saldo_cdi + aporte_mensal)*cdi_mensal
+        tempo -= 1
+
+    return saldo_cdi
+
+def tempo_para_renda_desejada(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, renda_passiva_desejada):
     
     saldo = valor_inicial
     meses = 0
@@ -77,10 +92,12 @@ def tempo_para_renda_desejada(valor_inicial, aporte_mensal, rendimento_anual, di
     meses_restantes = meses % 12
     
     print('Saldo: {:,.2f}'.format(saldo))
+    print('Em um investimento de 100% do CDI seu saldo seria: {:,.2f}'.format(comparar_ao_cdi(valor_inicial, aporte_mensal, anos, meses_restantes)))
+    print('Uma diferença de: {:,.2f}'.format(saldo - comparar_ao_cdi(valor_inicial, aporte_mensal, anos, meses_restantes)))
     print(f"Levará aproximadamente {anos} anos e {meses_restantes} meses para atingir a renda passiva desejada de R${renda_passiva_desejada:,.2f} mensais.")
     construcao_grafico(lista_aportado, lista_redimento, lista_renda_passiva)
 
-def renda_mensal_estimada_apos_anos(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, anos, meses, INTERVALO_COLUNAS):
+def renda_mensal_estimada_apos_anos(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, anos, meses):
     
     saldo = valor_inicial
     tempo = anos * 12 + meses
@@ -104,6 +121,8 @@ def renda_mensal_estimada_apos_anos(valor_inicial, aporte_mensal, rendimento_anu
         lista_renda_passiva.append(renda_passiva_mensal)
 
     print('Saldo: {:,.2f}'.format(saldo))
+    print('Em um investimento de 100% do CDI seu saldo seria: {:,.2f}'.format(comparar_ao_cdi(valor_inicial, aporte_mensal, anos, meses)))
+    print('Uma diferença de: {:,.2f}'.format(saldo - comparar_ao_cdi(valor_inicial, aporte_mensal, anos, meses)))
     print(f"A renda mensal estimada após {anos} anos e {meses} meses é de aproximadamente R${renda_passiva_mensal:,.2f}")
     construcao_grafico(lista_aportado, lista_redimento, lista_renda_passiva)
 
@@ -115,14 +134,16 @@ def renda_mensal_estimada_apos_anos(valor_inicial, aporte_mensal, rendimento_anu
 #renda_passiva_desejada = float(input("Informe a renda passiva desejada: "))
 
 valor_inicial = float(15000)
-aporte_mensal = float(1100)
-rendimento_anual = float(0.105)
+aporte_mensal = float(1000)
+rendimento_anual = float(0.10)
 dividend_yield_anual = float(0.069)
 renda_passiva_desejada = float(1320)
 anos_informados, meses_informados = int(0), int(0)
+#100% do CDI está em 0.1365
+TAXA_CDI = float(0.1365)
 INTERVALO_COLUNAS = int(12)
 
 if anos_informados == 0 and meses_informados == 0:
-    tempo_para_renda_desejada(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, renda_passiva_desejada, INTERVALO_COLUNAS)
+    tempo_para_renda_desejada(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, renda_passiva_desejada)
 else:
-    renda_mensal_estimada_apos_anos(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, anos_informados, meses_informados, INTERVALO_COLUNAS)
+    renda_mensal_estimada_apos_anos(valor_inicial, aporte_mensal, rendimento_anual, dividend_yield_anual, anos_informados, meses_informados)
